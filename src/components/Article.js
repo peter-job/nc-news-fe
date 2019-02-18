1667855ebeff2ea6c667606e364b12688ac87169
ptjob1 <ptjob6@gmail.com>
2019-02-18 18:51:00 +0000

    Add details and styles to Article component

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,8 +3,25 @@ import "../styles/Article.css";
 
 const Article = ({ article }) => {
   return (
-    <div>
-      <span>{article.title}</span>
+    <div className="Article">
+      <div className="avatar">
+        <img
+          alt={article.author}
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/SNice.svg/220px-SNice.svg.png"
+        />
+      </div>
+      <div className="author">
+        <span>{article.author}</span>
+      </div>
+      <div className="topic">
+        <span>{article.topic}</span>
+      </div>
+      <div className="title">
+        <span>{article.title}</span>
+      </div>
+      <div className="controls">
+        <span>votes: {article.votes}</span>
+      </div>
     </div>
   );
 };
