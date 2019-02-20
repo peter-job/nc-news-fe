2dbebaa43dda725540765271e94715cb52239272
ptjob1 <ptjob6@gmail.com>
2019-02-20 14:49:26 +0000

    Add api file with request methods

---
+++ src/api.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+const BASE_URL = "https://be2-nc-news.herokuapp.com/api/";
+
+export const fetchArticles = (sort_by = "created_at") => {
+  const path = `articles?sort_by=${sort_by}`;
+  return axios.get(`${BASE_URL + path}`).then(data => data.articles);
+};
+
+export const patchVotes = (article_id, comment_id, voted) => {
+  const body = { inc_votes: voted };
+  const path = `articles/${article_id}${
+    comment_id ? `/comments/${comment_id}` : ""
+  }`;
+  return axios.patch(`${BASE_URL + path}`, body);
+};
