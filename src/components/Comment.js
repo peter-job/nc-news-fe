29da6b10f08993ca90409ea61abdb8727d9616c0
ptjob1 <ptjob6@gmail.com>
2019-02-22 12:34:19 +0000

    Create full Comment component with same structure as Article

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,10 +1,34 @@
 import React from "react";
+import "../styles/Comment.css";
+import Moment from "moment";
+import Votes from "./Votes";
 
-const Comment = ({ comment }) => {
+const Comment = ({ comment, article_id }) => {
   return (
     <div className="Comment">
-      <p>{comment.author}</p>
-      <p>{comment.body}</p>
+      <div className="avatar">
+        <img
+          alt={comment.author}
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/SNice.svg/220px-SNice.svg.png"
+        />
+      </div>
+      <div className="author">
+        <span>
+          {comment.author}
+          <br />
+          {Moment(comment.created_at, "YYYY-MM-DD-Thh:mm:ss").fromNow()}
+        </span>
+      </div>
+      <div className="body">
+        <p>{comment.body}</p>
+      </div>
+      <div className="Votes">
+        <Votes
+          votes={comment.votes}
+          article_id={article_id}
+          comment_id={comment.comment_id}
+        />
+      </div>
     </div>
   );
 };
